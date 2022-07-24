0eb78c639ee4271a3b6792aa285031ad65b91d0b
gnlien21 <gnlien21@gmail.com>
2022-07-24 21:01:08 +0700

    push bai lan 2

--- controller/ajax.js
+++ controller/ajax.js
@@ -1,18 +1,18 @@
 //Get
 
-function layDuLieuSanPhamApi (){
+function layDuLieuSanPhamApi() {
     var promise = axios({
-        url:'http://svcy.myclass.vn/api/Product/GetAll',
+        url: 'http://svcy.myclass.vn/api/Product/GetAll',
         method: 'GET',
     });
 
-    promise.then(function(result){
+    promise.then(function (result) {
         console.log(result.data);
         renderDSSanPham(result.data);
     });
 
-    promise.catch(function(error){
-        console.log(error);
+    promise.catch(function (err) {
+        console.log(err);
     })
 }
 
@@ -20,7 +20,7 @@ function layDuLieuSanPhamApi (){
 
 //Create product
 
-document.querySelector('#btnCreate').onclick = function(){
+document.querySelector('#btnCreate').onclick = function () {
     var sp = new SanPham();
 
     sp.id = document.querySelector('#productId').value;
@@ -33,21 +33,100 @@ document.querySelector('#btnCreate').onclick = function(){
     console.log(sp);
 
     var promise = axios({
-        url:'http://svcy.myclass.vn/api/Product/CreateProduct',
+        url: 'http://svcy.myclass.vn/api/Product/CreateProduct',
         method: 'POST',
         data: sp
     });
 
-    promise.then(function(result){
+    promise.then(function (result) {
         console.log(result.data);
-        renderDSSanPham(result.data);
+        layDuLieuSanPhamApi();
     });
 
-    promise.catch(function(err){
-        console.log(error);
+    promise.catch(function (err) {
+        console.log(err);
     });
 }
 
-window.onload = function (){
-    renderDSSanPham();
+window.onload = function () {
+    layDuLieuSanPhamApi();
+}
+
+//delete product
+
+function xoaSanPham(idSPClick) {
+    alert(idSPClick);
+    var promise = axios({
+        url: 'http://svcy.myclass.vn/api/Product/DeleteProduct/' + idSPClick,
+        method: 'DELETE'
+    });
+
+    //Thành công:
+
+    promise.then(function (result) {
+        console.log("xoa san pham thanh cong!");
+        layDuLieuSanPhamApi();
+    });
+
+    //Thất bại
+
+    promise.catch(function (err) {
+        console.log(err);
+    });
+}
+
+//edit product
+
+function editSanPham(id) {
+    var promise = axios({
+        url: 'http://svcy.myclass.vn/api/Product/GetById/' + id,
+        method: 'GET',
+    });
+
+    //Thành công thì đem info sản phẩm load lên các thẻ
+
+    promise.then(function (result) {
+        var sp = result.data;
+
+        document.querySelector('#productId').value = sp.id;
+        document.querySelector('#productImage').value = sp.img;
+        document.querySelector('#productName').value = sp.name;
+        document.querySelector('#productPrice').value = sp.price;
+        document.querySelector('#productDescription').value = sp.description;
+        document.querySelector('#productType').value = sp.type;
+    });
+
+    //Thất bại
+
+    promise.catch(function(error){
+        console.log(error);
+    });
+
+}
+
+//Put: Cập nhật dữ liệu
+
+document.querySelector('#btnUpdate').onclick = function(){
+    var spUpdate = new SanPham();
+
+    spUpdate.id = document.querySelector('#productId').value;
+    spUpdate.img = document.querySelector('#productImage').value;
+    spUpdate.name = document.querySelector('#productName').value;
+    spUpdate.price = document.querySelector('#productPrice').value;
+    spUpdate.description = document.querySelector('#productDescription').value;
+    spUpdate.type = document.querySelector('#productType').value;
+
+    var promise = axios({
+        url:'http://svcy.myclass.vn/api/Product/UpdateProduct'+spUpdate.id,
+        method: 'PUT',
+        data: spUpdate
+    });
+
+    promise.then(function(){
+        layDuLieuSanPhamApi();
+    });
+
+    promise.catch(function(error){
+        console.log(error);
+    });
 }
--- util/method.js
+++ util/method.js
@@ -6,15 +6,19 @@ function renderDSSanPham (arrSanPham){
 
             <tr>
             <td>${sp.id}</td>
-            <td>${sp.img}</td>
+            <td><img src="${sp.img}"  width="150" height="150"/></td>
             <td>${sp.name}</td>
             <td>${sp.price}</td>
             <td>${sp.description}</td>
             <td>${sp.type}</td>
-            <button class="btn btn-danger" id="btnXoa"><i class="fa-solid fa-trash-can"></i></button>
-            <button class="btn btn-primary" id="btnChinhSua"> <i class="fa-solid fa-pen-to-square"></i></button>
+            <td><button class="btn btn-danger" id="btnXoa" onclick="xoaSanPham('${sp.id}')">Xóa</button></td>
+            <td><button class="btn btn-primary" id="btnChinhSua" onclick="editSanPham('${sp.id}')">Sửa</button></td>
             </tr>
         `;
     }
     document.querySelector('#tblSanPham').innerHTML = html;
 }
+
+//<i class="fa-solid fa-trash-can"></i>
+//<i class="fa-solid fa-pen-to-square"></i>
+
